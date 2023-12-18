2d6cca0f92fdf7973cb16ace0e4bf5228e70a80d
Jennifer Struthers <jennifer.struthers1@nhs.net>
2023-12-18 12:27:16 +0000

    added some functions

--- my_simple_functions.py
+++ my_simple_functions.py
@@ -12,3 +12,9 @@ def divide_numbers(a, b):
         return a / b
     else:
         return "Cannot divide by zero!"
+
+def multiply_many_numbers(a, b, c, d, e):
+    return a*b*c*d*e
+
+def add_one(a):
+    return a + 1
